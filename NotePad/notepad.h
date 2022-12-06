61583e8b8e78bf702275e06498bd5d7ae1c7349e
Advanced-CPP1 <117765687+Advanced-CPP1@users.noreply.github.com>
2022-12-06 19:44:28 +0000

    Add files via upload

---
+++ NotePad/notepad.h
@@ -0,0 +1,60 @@
+#ifndef NOTEPAD_H
+#define NOTEPAD_H
+
+#include <QMainWindow>
+#include <QColorDialog>
+#include <QKeyEvent>
+
+QT_BEGIN_NAMESPACE
+namespace Ui { class NotePad; }
+QT_END_NAMESPACE
+
+class NotePad : public QMainWindow
+{
+    Q_OBJECT
+
+public:
+    NotePad(QWidget *parent = nullptr);
+    ~NotePad();
+
+private slots:
+    void on_fontComboBox_currentFontChanged(const QFont &f);
+
+    void on_comboBox_activated(int index);
+
+    void on_color_activated(int index);
+
+    void on_bold_button_clicked();
+
+    void on_bold_button_2_clicked();
+
+    void on_underline_button_clicked();
+
+    void on_underline_button_2_clicked();
+
+    void on_center_button_clicked();
+
+    void on_italic_button_clicked();
+
+    void on_center_button_2_clicked();
+
+    void on_italic_button_2_clicked();
+
+    void on_linethrough_clicked();
+
+    void on_linethrough_2_clicked();
+
+    void on_paste_button_clicked();
+
+    void on_copy_button_clicked();
+
+    void on_cut_button_clicked();
+
+    void on_actionExit_triggered();
+
+    void on_lineEdit_textChanged(const QString &arg1);
+
+private:
+    Ui::NotePad *ui;
+};
+#endif // NOTEPAD_H
