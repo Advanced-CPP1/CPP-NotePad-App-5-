2fe7a44bb60e15947a9415d78bf8287d75a94f2c
Advanced-CPP1 <117765687+Advanced-CPP1@users.noreply.github.com>
2022-12-06 21:29:04 +0000

    Add files via upload

---
+++ NotePad software/moc_notepad.cpp
@@ -0,0 +1,277 @@
+/****************************************************************************
+** Meta object code from reading C++ file 'notepad.h'
+**
+** Created by: The Qt Meta Object Compiler version 68 (Qt 6.4.0)
+**
+** WARNING! All changes made in this file will be lost!
+*****************************************************************************/
+
+#include <memory>
+#include "../../New App/NotePad/notepad.h"
+#include <QtCore/qmetatype.h>
+#if !defined(Q_MOC_OUTPUT_REVISION)
+#error "The header file 'notepad.h' doesn't include <QObject>."
+#elif Q_MOC_OUTPUT_REVISION != 68
+#error "This file was generated using the moc from 6.4.0. It"
+#error "cannot be used with the include files from this version of Qt."
+#error "(The moc has changed too much.)"
+#endif
+
+#ifndef Q_CONSTINIT
+#define Q_CONSTINIT
+#endif
+
+QT_BEGIN_MOC_NAMESPACE
+QT_WARNING_PUSH
+QT_WARNING_DISABLE_DEPRECATED
+namespace {
+struct qt_meta_stringdata_NotePad_t {
+    uint offsetsAndSizes[46];
+    char stringdata0[8];
+    char stringdata1[35];
+    char stringdata2[1];
+    char stringdata3[2];
+    char stringdata4[22];
+    char stringdata5[6];
+    char stringdata6[19];
+    char stringdata7[23];
+    char stringdata8[25];
+    char stringdata9[28];
+    char stringdata10[30];
+    char stringdata11[25];
+    char stringdata12[25];
+    char stringdata13[27];
+    char stringdata14[27];
+    char stringdata15[23];
+    char stringdata16[25];
+    char stringdata17[24];
+    char stringdata18[23];
+    char stringdata19[22];
+    char stringdata20[24];
+    char stringdata21[24];
+    char stringdata22[5];
+};
+#define QT_MOC_LITERAL(ofs, len) \
+    uint(sizeof(qt_meta_stringdata_NotePad_t::offsetsAndSizes) + ofs), len 
+Q_CONSTINIT static const qt_meta_stringdata_NotePad_t qt_meta_stringdata_NotePad = {
+    {
+        QT_MOC_LITERAL(0, 7),  // "NotePad"
+        QT_MOC_LITERAL(8, 34),  // "on_fontComboBox_currentFontCh..."
+        QT_MOC_LITERAL(43, 0),  // ""
+        QT_MOC_LITERAL(44, 1),  // "f"
+        QT_MOC_LITERAL(46, 21),  // "on_comboBox_activated"
+        QT_MOC_LITERAL(68, 5),  // "index"
+        QT_MOC_LITERAL(74, 18),  // "on_color_activated"
+        QT_MOC_LITERAL(93, 22),  // "on_bold_button_clicked"
+        QT_MOC_LITERAL(116, 24),  // "on_bold_button_2_clicked"
+        QT_MOC_LITERAL(141, 27),  // "on_underline_button_clicked"
+        QT_MOC_LITERAL(169, 29),  // "on_underline_button_2_clicked"
+        QT_MOC_LITERAL(199, 24),  // "on_center_button_clicked"
+        QT_MOC_LITERAL(224, 24),  // "on_italic_button_clicked"
+        QT_MOC_LITERAL(249, 26),  // "on_center_button_2_clicked"
+        QT_MOC_LITERAL(276, 26),  // "on_italic_button_2_clicked"
+        QT_MOC_LITERAL(303, 22),  // "on_linethrough_clicked"
+        QT_MOC_LITERAL(326, 24),  // "on_linethrough_2_clicked"
+        QT_MOC_LITERAL(351, 23),  // "on_paste_button_clicked"
+        QT_MOC_LITERAL(375, 22),  // "on_copy_button_clicked"
+        QT_MOC_LITERAL(398, 21),  // "on_cut_button_clicked"
+        QT_MOC_LITERAL(420, 23),  // "on_actionExit_triggered"
+        QT_MOC_LITERAL(444, 23),  // "on_lineEdit_textChanged"
+        QT_MOC_LITERAL(468, 4)   // "arg1"
+    },
+    "NotePad",
+    "on_fontComboBox_currentFontChanged",
+    "",
+    "f",
+    "on_comboBox_activated",
+    "index",
+    "on_color_activated",
+    "on_bold_button_clicked",
+    "on_bold_button_2_clicked",
+    "on_underline_button_clicked",
+    "on_underline_button_2_clicked",
+    "on_center_button_clicked",
+    "on_italic_button_clicked",
+    "on_center_button_2_clicked",
+    "on_italic_button_2_clicked",
+    "on_linethrough_clicked",
+    "on_linethrough_2_clicked",
+    "on_paste_button_clicked",
+    "on_copy_button_clicked",
+    "on_cut_button_clicked",
+    "on_actionExit_triggered",
+    "on_lineEdit_textChanged",
+    "arg1"
+};
+#undef QT_MOC_LITERAL
+} // unnamed namespace
+
+Q_CONSTINIT static const uint qt_meta_data_NotePad[] = {
+
+ // content:
+      10,       // revision
+       0,       // classname
+       0,    0, // classinfo
+      18,   14, // methods
+       0,    0, // properties
+       0,    0, // enums/sets
+       0,    0, // constructors
+       0,       // flags
+       0,       // signalCount
+
+ // slots: name, argc, parameters, tag, flags, initial metatype offsets
+       1,    1,  122,    2, 0x08,    1 /* Private */,
+       4,    1,  125,    2, 0x08,    3 /* Private */,
+       6,    1,  128,    2, 0x08,    5 /* Private */,
+       7,    0,  131,    2, 0x08,    7 /* Private */,
+       8,    0,  132,    2, 0x08,    8 /* Private */,
+       9,    0,  133,    2, 0x08,    9 /* Private */,
+      10,    0,  134,    2, 0x08,   10 /* Private */,
+      11,    0,  135,    2, 0x08,   11 /* Private */,
+      12,    0,  136,    2, 0x08,   12 /* Private */,
+      13,    0,  137,    2, 0x08,   13 /* Private */,
+      14,    0,  138,    2, 0x08,   14 /* Private */,
+      15,    0,  139,    2, 0x08,   15 /* Private */,
+      16,    0,  140,    2, 0x08,   16 /* Private */,
+      17,    0,  141,    2, 0x08,   17 /* Private */,
+      18,    0,  142,    2, 0x08,   18 /* Private */,
+      19,    0,  143,    2, 0x08,   19 /* Private */,
+      20,    0,  144,    2, 0x08,   20 /* Private */,
+      21,    1,  145,    2, 0x08,   21 /* Private */,
+
+ // slots: parameters
+    QMetaType::Void, QMetaType::QFont,    3,
+    QMetaType::Void, QMetaType::Int,    5,
+    QMetaType::Void, QMetaType::Int,    5,
+    QMetaType::Void,
+    QMetaType::Void,
+    QMetaType::Void,
+    QMetaType::Void,
+    QMetaType::Void,
+    QMetaType::Void,
+    QMetaType::Void,
+    QMetaType::Void,
+    QMetaType::Void,
+    QMetaType::Void,
+    QMetaType::Void,
+    QMetaType::Void,
+    QMetaType::Void,
+    QMetaType::Void,
+    QMetaType::Void, QMetaType::QString,   22,
+
+       0        // eod
+};
+
+Q_CONSTINIT const QMetaObject NotePad::staticMetaObject = { {
+    QMetaObject::SuperData::link<QMainWindow::staticMetaObject>(),
+    qt_meta_stringdata_NotePad.offsetsAndSizes,
+    qt_meta_data_NotePad,
+    qt_static_metacall,
+    nullptr,
+    qt_incomplete_metaTypeArray<qt_meta_stringdata_NotePad_t,
+        // Q_OBJECT / Q_GADGET
+        QtPrivate::TypeAndForceComplete<NotePad, std::true_type>,
+        // method 'on_fontComboBox_currentFontChanged'
+        QtPrivate::TypeAndForceComplete<void, std::false_type>,
+        QtPrivate::TypeAndForceComplete<const QFont &, std::false_type>,
+        // method 'on_comboBox_activated'
+        QtPrivate::TypeAndForceComplete<void, std::false_type>,
+        QtPrivate::TypeAndForceComplete<int, std::false_type>,
+        // method 'on_color_activated'
+        QtPrivate::TypeAndForceComplete<void, std::false_type>,
+        QtPrivate::TypeAndForceComplete<int, std::false_type>,
+        // method 'on_bold_button_clicked'
+        QtPrivate::TypeAndForceComplete<void, std::false_type>,
+        // method 'on_bold_button_2_clicked'
+        QtPrivate::TypeAndForceComplete<void, std::false_type>,
+        // method 'on_underline_button_clicked'
+        QtPrivate::TypeAndForceComplete<void, std::false_type>,
+        // method 'on_underline_button_2_clicked'
+        QtPrivate::TypeAndForceComplete<void, std::false_type>,
+        // method 'on_center_button_clicked'
+        QtPrivate::TypeAndForceComplete<void, std::false_type>,
+        // method 'on_italic_button_clicked'
+        QtPrivate::TypeAndForceComplete<void, std::false_type>,
+        // method 'on_center_button_2_clicked'
+        QtPrivate::TypeAndForceComplete<void, std::false_type>,
+        // method 'on_italic_button_2_clicked'
+        QtPrivate::TypeAndForceComplete<void, std::false_type>,
+        // method 'on_linethrough_clicked'
+        QtPrivate::TypeAndForceComplete<void, std::false_type>,
+        // method 'on_linethrough_2_clicked'
+        QtPrivate::TypeAndForceComplete<void, std::false_type>,
+        // method 'on_paste_button_clicked'
+        QtPrivate::TypeAndForceComplete<void, std::false_type>,
+        // method 'on_copy_button_clicked'
+        QtPrivate::TypeAndForceComplete<void, std::false_type>,
+        // method 'on_cut_button_clicked'
+        QtPrivate::TypeAndForceComplete<void, std::false_type>,
+        // method 'on_actionExit_triggered'
+        QtPrivate::TypeAndForceComplete<void, std::false_type>,
+        // method 'on_lineEdit_textChanged'
+        QtPrivate::TypeAndForceComplete<void, std::false_type>,
+        QtPrivate::TypeAndForceComplete<const QString &, std::false_type>
+    >,
+    nullptr
+} };
+
+void NotePad::qt_static_metacall(QObject *_o, QMetaObject::Call _c, int _id, void **_a)
+{
+    if (_c == QMetaObject::InvokeMetaMethod) {
+        auto *_t = static_cast<NotePad *>(_o);
+        (void)_t;
+        switch (_id) {
+        case 0: _t->on_fontComboBox_currentFontChanged((*reinterpret_cast< std::add_pointer_t<QFont>>(_a[1]))); break;
+        case 1: _t->on_comboBox_activated((*reinterpret_cast< std::add_pointer_t<int>>(_a[1]))); break;
+        case 2: _t->on_color_activated((*reinterpret_cast< std::add_pointer_t<int>>(_a[1]))); break;
+        case 3: _t->on_bold_button_clicked(); break;
+        case 4: _t->on_bold_button_2_clicked(); break;
+        case 5: _t->on_underline_button_clicked(); break;
+        case 6: _t->on_underline_button_2_clicked(); break;
+        case 7: _t->on_center_button_clicked(); break;
+        case 8: _t->on_italic_button_clicked(); break;
+        case 9: _t->on_center_button_2_clicked(); break;
+        case 10: _t->on_italic_button_2_clicked(); break;
+        case 11: _t->on_linethrough_clicked(); break;
+        case 12: _t->on_linethrough_2_clicked(); break;
+        case 13: _t->on_paste_button_clicked(); break;
+        case 14: _t->on_copy_button_clicked(); break;
+        case 15: _t->on_cut_button_clicked(); break;
+        case 16: _t->on_actionExit_triggered(); break;
+        case 17: _t->on_lineEdit_textChanged((*reinterpret_cast< std::add_pointer_t<QString>>(_a[1]))); break;
+        default: ;
+        }
+    }
+}
+
+const QMetaObject *NotePad::metaObject() const
+{
+    return QObject::d_ptr->metaObject ? QObject::d_ptr->dynamicMetaObject() : &staticMetaObject;
+}
+
+void *NotePad::qt_metacast(const char *_clname)
+{
+    if (!_clname) return nullptr;
+    if (!strcmp(_clname, qt_meta_stringdata_NotePad.stringdata0))
+        return static_cast<void*>(this);
+    return QMainWindow::qt_metacast(_clname);
+}
+
+int NotePad::qt_metacall(QMetaObject::Call _c, int _id, void **_a)
+{
+    _id = QMainWindow::qt_metacall(_c, _id, _a);
+    if (_id < 0)
+        return _id;
+    if (_c == QMetaObject::InvokeMetaMethod) {
+        if (_id < 18)
+            qt_static_metacall(this, _c, _id, _a);
+        _id -= 18;
+    } else if (_c == QMetaObject::RegisterMethodArgumentMetaType) {
+        if (_id < 18)
+            *reinterpret_cast<QMetaType *>(_a[0]) = QMetaType();
+        _id -= 18;
+    }
+    return _id;
+}
+QT_WARNING_POP
+QT_END_MOC_NAMESPACE
